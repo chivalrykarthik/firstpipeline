3b0fa0a2cbacb88a6330f37c23999a5e9f44105f
Karthik Arumugam <chivalrykarthik@gmail.com>
2020-12-27 22:21:23 +0530

    Static site content added

--- scripts/npm-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/firstpipeline
-git pull origin master
-npm install
--- scripts/npm-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/firstpipeline
-sudo -H -u ec2-user bash -c 'npm start'
